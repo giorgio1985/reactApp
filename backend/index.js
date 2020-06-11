5cbe45196cf30df4fa2fd33c7c4b937dc56bb5e0
giorgio1985 <g.adonoo@yahoo.it>
2020-06-11 15:01:14 +0200

    Change port number

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 var http = require('http');
 
-var port = 8080;
+var port = 8000;
+ console.log('Ciao, imposta la connessione alla porta numero ' + port);
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
-  res.end('Ciao a tutti, sono un web server!');
+  res.end("Benvenuto nel mio server");
   console.log("Server running at the port = " + port);
 });
 server.listen(port);
