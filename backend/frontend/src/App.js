6edf1a1a300aecfdb6128787b100fb212cbfcd6b
giorgio1985 <g.adonoo@yahoo.it>
2020-06-12 19:58:16 +0200

    change paragraph

--- backend/frontend/src/App.js
+++ backend/frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Prima pagina con react js.
+          Prima Home Page con react js!
         </p>
        
       </header>
