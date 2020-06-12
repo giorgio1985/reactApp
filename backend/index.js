59933bca7ca46a64fef80fbafa79f2535a691a27
giorgio1985 <g.adonoo@yahoo.it>
2020-06-12 19:17:22 +0200

    add module node js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 var http = require('http');
 
 var port = 8000;
- console.log('Ciao, imposta la connessione alla porta numero ' + port);
+ console.log('Ciao, imposta la connessione alla porta numero ' + port + "\n");
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
-  res.end("Benvenuto nel mio server");
-  console.log("Server running at the port = " + port);
+  res.end("Benvenuto nel mio server\n");
+  console.log("Server running at the port = " + port + "\n");
 });
 server.listen(port);
