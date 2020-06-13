55f7bb49137e8e9367cede400a8e1c41b466d16c
giorgio1985 <34795529+giorgio1985@users.noreply.github.com>
2020-06-13 16:13:16 +0200

    update

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ var port = 8000;
  console.log('Ciao, imposta la connessione alla porta numero ' + port + "\n");
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
-  res.end("Benvenuto nel mio server\n");
+  res.end("Benvenuto nel mio server " + new Date() + "\n");
   console.log("Server running at the port = " + port + "\n");
 });
 server.listen(port);
