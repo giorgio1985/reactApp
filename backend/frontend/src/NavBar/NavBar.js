a4064c31744ce676029e123c783d720e5dab79d8
giorgio1985 <g.adonoo@yahoo.it>
2020-06-15 14:44:02 +0200

    adding NavBar module

---
+++ backend/frontend/src/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './NavBar.css';
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <header className="NavBar-header">
+        <p>
+         Questa e la mia navbar!
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default NavBar;
