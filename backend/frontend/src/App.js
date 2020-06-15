4af0ee8203c49b61b1a69fb0f077df11ad33daeb
giorgio1985 <g.adonoo@yahoo.it>
2020-06-15 15:57:02 +0200

    set new classes

--- backend/frontend/src/App.js
+++ backend/frontend/src/App.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends Component {
+ render() {
+ return (
+<div className="App">
+ <header className="App-header">
+   <img src={logo} className="App-logo" alt="logo" />
         <p>
           Prima Home Page con react js!
         </p>
        
-      </header>
-    </div>
-  );
+   </header>
+</div>
+);
 }
+}
+
+
 
 export default App;
--- backend/frontend/src/NavBar/NavBar.js
+++ backend/frontend/src/NavBar/NavBar.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './NavBar.css';
 
-function NavBar() {
-  return (
+class NavBar extends Component {
+ render() {
+ return (
     <div className="NavBar">
       <header className="NavBar-header">
         <p>
          Questa e la mia navbar!
         </p>
-       
       </header>
     </div>
   );
+
+}
+ 
 }
 
 export default NavBar;
